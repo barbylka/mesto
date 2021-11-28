a967a95fb52ed326b17f5e8a29d0c073d99f2d07
Irina Stafeeva <irina.staffeewa@gmail.com>
2021-11-28 18:42:57 +0300

    fix: correct input value

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,7 +9,6 @@ for (let i = 0; i < allLikes.length; i++) {
   })
 }
 
-
 let editBtn = document.querySelector('.profile__cont-edit-button');
 let popup = document.querySelector('.popup');
 let body = document.querySelector('.root');
@@ -20,22 +19,16 @@ function openPopup() {
   body.classList.add('root_hidden');
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
-  body.classList.remove('root_hidden');
-}
-
 editBtn.addEventListener('click', openPopup);
-exitBtn.addEventListener('click', closePopup);
 
 let formElement = document.querySelector('.popup__container');
-console.log(formElement);
-
 let header = document.querySelector('.profile__cont-info-name');
-console.log(header.textContent);
 let job = document.querySelector('.profile__cont-info-description');
 let nameInput = document.querySelector('.popup__text_type_name');
 let jobInput = document.querySelector('.popup__text_type_description');
+
+nameInput.value = header.textContent;
+jobInput.value = job.textContent;
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -49,3 +42,11 @@ function formSubmitHandler (evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 formElement.addEventListener('submit', closePopup);
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  body.classList.remove('root_hidden');
+  nameInput.value = header.textContent;
+  jobInput.value = job.textContent;
+}
+exitBtn.addEventListener('click', closePopup);
