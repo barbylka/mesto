be4e86258e4b475cf94815aa42fb5d6bf17da393
Irina Stafeeva <irina.staffeewa@gmail.com>
2021-12-01 22:47:52 +0300

    refactor: popup js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ function touchLike() {
   let allLikes = document.querySelectorAll('.places__like');
   for (let i = 0; i < allLikes.length; i++) {
     const likeIcon = allLikes[i];
-    likeIcon.addEventListener('click', function() {
+    likeIcon.addEventListener('click', function () {
       if (likeIcon.classList.contains('places__like_active')) {
         likeIcon.classList.remove('places__like_active');
       } else {
@@ -17,41 +17,36 @@ touchLike();
 let editBtn = document.querySelector('.profile__button_type_edit');
 let popup = document.querySelector('.popup');
 let body = document.querySelector('.root');
-let exitBtn = document.querySelector('.popup__exit-button');
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  body.classList.add('root_hidden');
-}
-
-editBtn.addEventListener('click', openPopup);
-
-let formElement = document.querySelector('.popup__container');
 let header = document.querySelector('.profile__cont-info-name');
 let job = document.querySelector('.profile__cont-info-description');
 let nameInput = document.querySelector('.popup__text_type_name');
 let jobInput = document.querySelector('.popup__text_type_description');
+let exitBtn = document.querySelector('.popup__exit-button');
 
-nameInput.value = header.textContent;
-jobInput.value = job.textContent;
+function togglePopup() {
+  if (!popup.classList.contains('popup_opened')) {
+    nameInput.value = header.textContent;
+    jobInput.value = job.textContent;
+  }
+  
+  popup.classList.toggle('popup_opened');
+  body.classList.toggle('root_hidden');
+}
 
-function formSubmitHandler (evt) {
+editBtn.addEventListener('click', togglePopup);
+exitBtn.addEventListener('click', togglePopup);
+
+let formElement = document.querySelector('.popup__container');
+
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   nameInput.getAttribute('value');
   jobInput.getAttribute('value');
-  
+
   header.textContent = nameInput.value;
   job.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', closePopup);
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-  body.classList.remove('root_hidden');
-  nameInput.value = header.textContent;
-  jobInput.value = job.textContent;
-}
-exitBtn.addEventListener('click', closePopup);
+formElement.addEventListener('submit', togglePopup);
